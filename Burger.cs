2f4dd1aafe40910a38749f9ae85a3b5007db63bd
Jax <93849152+StarFallenJax@users.noreply.github.com>
2023-10-12 08:44:22 -0700

    Added base files 1

---
+++ Burger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Alexandria.ItemAPI;
+
+namespace Burger
+{
+    public class Burger : PassiveItem
+    {
+        int shieldOnPickup = 1;
+        int shieldOnFloorCleared = 1;
+        int shieldOnFloorClearedTotal = 0;
+        //Call this method from the Start() method of your ETGModule extension
+        public static void Register()
+        {
+            //The name of the item
+            string itemName = "B.U.R.G.E.R.";
+
+            //Refers to an embedded png in the project. Make sure to embed your resources! Google it
+            string resourceName = "Burger/Resources/burger";
+
+            //Create new GameObject
+            GameObject obj = new GameObject(itemName);
+
+            //Add a PassiveItem component to the object
+            var item = obj.AddComponent<Burger>();
+
+            //Adds a sprite component to the object and adds your texture to the item sprite collection
+            ItemBuilder.AddSpriteToObject(itemName, resourceName, obj);
+
+            //Ammonomicon entry variables
+            string shortDesc = "Long Acronyms Taste Better";
+            string longDesc = "The B.U.R.G.E.R. is also known as fast food.\n\n" +
+                "This beautifully hand-crafted B.U.R.G.E.R. stands for Barrier Utilizing Reinforcement Generating Energy Reserves and gives the gungeonier shield every floor. Thank goodness for unsustainable food production practiced by companies that strive only to make an extra dime!";
+
+            //Adds the item to the gungeon item list, the ammonomicon, the loot table, etc.
+            //Do this after ItemBuilder.AddSpriteToObject!
+            ItemBuilder.SetupItem(item, shortDesc, longDesc, "bur");
+
+            //Adds the actual passive effect to the item
+            //ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Health, 1, StatModifier.ModifyMethod.ADDITIVE);
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.Coolness, 1);
+
+            //Set the rarity of the item
+            item.quality = PickupObject.ItemQuality.B;
+
+        }
+
+        public override void Pickup(PlayerController player)
+        {
+            base.Pickup(player);
+            player.healthHaver.Armor += shieldOnPickup;
+            Module.Log($"Player picked up {DisplayName}");
+            player.OnNewFloorLoaded += NewFloorLoaded;
+
+        }
+
+        public override void DisableEffect(PlayerController player)
+        {
+            Module.Log($"Player dropped or got rid of B.U.R.G.E.R.");
+        }
+        
+        public override DebrisObject Drop(PlayerController player)
+        {
+            player.OnNewFloorLoaded -= this.NewFloorLoaded;
+            player.healthHaver.Armor -= shieldOnPickup;
+            player.healthHaver.Armor -= shieldOnFloorClearedTotal;
+            return base.Drop(player);
+        }
+
+        bool half_toggle = false;
+        public void NewFloorLoaded(PlayerController player)
+        {
+            if (half_toggle)
+            {
+                player.healthHaver.Armor += shieldOnFloorCleared;
+                half_toggle = false;
+                shieldOnFloorClearedTotal++;
+            }
+            else
+            {
+                half_toggle = true;
+            }
+          
+        }
+
+    }
+}
---
+++ Module.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using Alexandria;
+using Alexandria.ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Burger
+{
+    [BepInDependency(Alexandria.Alexandria.GUID)] // this mod depends on the Alexandria API: https://enter-the-gungeon.thunderstore.io/package/Alexandria/Alexandria/
+    [BepInDependency(ETGModMainBehaviour.GUID)]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Module : BaseUnityPlugin
+    {
+        public const string GUID = "starfallenjax.etg.burger";
+        public const string NAME = "B.U.R.G.E.R.";
+        public const string VERSION = "1.0.0";
+        public const string TEXT_COLOR = "#00FFFF";
+
+        public void Start()
+        {
+            ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
+        }
+
+        public void GMStart(GameManager g)
+        {
+            Burger.Register();
+            Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+        }
+
+        public static void Log(string text, string color="#FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+    }
+}
